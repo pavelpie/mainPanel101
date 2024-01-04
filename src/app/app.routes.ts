86ec34b983e3787f9025e8f54ad890d2159a767d
pawel <pavelpie@gmail.com>
2024-01-04 10:43:39 +0100

    Pierwsze prace nad głównym panelem zarządzania

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {UsersComponent} from "./main/users/users.component";
+import {MainComponent} from "./main/main.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/app/main', pathMatch: 'full'},
+  {path: 'app', component: AppComponent, children: [
+      {path: 'main', component: MainComponent, children: [
+          { path: 'users', component: UsersComponent }
+        ]
+      }
+    ]},
+];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
