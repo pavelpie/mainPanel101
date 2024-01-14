d1033b21570c89a07e7d668de180bf05b93eb5e5
grzejatol <grzejatol@gmail.com>
2024-01-14 20:22:07 +0000

    zrobiona strona users

--- src/app/main/users/users.component.ts
+++ src/app/main/users/users.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+ usersList = [
+    {name: 'John', age: 25, email: 'gfgdsgds@gsdg'},
+    {name: 'Bon', age: 30, email: 'rodi@gsdg'},
+    {name: 'Krom', age: 30, email: 'wiwi@gsdg'}
+ ]
+
 
 }
