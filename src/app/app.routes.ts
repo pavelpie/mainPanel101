f5f2a2dfc52ce24b5a8369f21adaad3b047c0543
grzejatol <grzejatol@gmail.com>
2024-01-16 20:18:02 +0000

    zrobiona strona users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import {AppComponent} from "./app.component";
 import {UsersComponent} from "./main/users/users.component";
 import {MainComponent} from "./main/main.component";
+import {UserComponent} from "./main/users/user/user.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/app/main', pathMatch: 'full'},
   {path: 'app', component: AppComponent, children: [
       {path: 'main', component: MainComponent, children: [
-          { path: 'users', component: UsersComponent }
+          { path: 'users', component: UsersComponent },
+          { path: 'user', component: UserComponent }
         ]
       }
     ]},
--- src/app/main/users/users.component.ts
+++ src/app/main/users/users.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NgStyle} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [
-    NgStyle
+    NgStyle,
+    RouterLink
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
